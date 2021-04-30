24f3f38724533bc0fbc0a408efc42c3fa63e80fe
Mauro Melo <mauromelo1@gmail.com>
2021-04-30 16:34:16 -0300

    Projeto criado

---
+++ src/main/java/com/mauromelo1/curso/boot/DemoMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.mauromelo1.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauromelo1/curso/boot/DemoMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauromelo1.curso.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
